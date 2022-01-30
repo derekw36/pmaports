f4febc82b74f3a1bfa8e59d029c4a4f42f9327e0
Derek Watson <derekw1996@gmail.com>
2022-01-30 15:57:31 -0500

    sanders: display works
    Needed to disable some kernel options that bound to the framebuffer,
    and then add a patch that would update the fb mode.

---
+++ device/testing/device-motorola-sanders/initfs-hook.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# enable touchscreen - not needed for X11 since it's enabled automatically
+#sleep 5 # a little delay before activating touchscreen
+#echo 1 > /sys/devices/soc/78b7000.i2c/i2c-3/3-0038/drv_irq
